11f5271a2035798895fe1d774d9a809e6d7edf60
otatsu <dorado727@icloud.com>
2024-04-15 10:35:02 +0900

    Swift チュートリアル

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -1,17 +1,13 @@
-//
-//  LandmarksApp.swift
-//  Landmarks
-//
-//  Created by tatsuya eguchi on 2024/04/15.
-//
-
 import SwiftUI
 
 @main
 struct LandmarksApp: App {
+    @State private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(modelData)
         }
     }
 }

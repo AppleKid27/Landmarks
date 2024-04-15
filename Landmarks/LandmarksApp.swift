4bbc14d32bcbcf7c6a2eefc046e97e1c9e41ee05
otatsu <dorado727@icloud.com>
2024-04-15 10:19:55 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by tatsuya eguchi on 2024/04/15.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
